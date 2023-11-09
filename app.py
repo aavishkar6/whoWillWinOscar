b03eeb958cd0cd60d2e6b24760eb781d80cf48d0
Aavishkar <ag8298@nyu.edu>
2023-11-09 16:42:38 -0500

    handle no search condition

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
+# import required modules and database.
+import base64
+import json
+from io import BytesIO
+import matplotlib.pyplot as plt
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+
+# import functions for reading and writing from the database
 from db import (
     create_sql_engine,
     get_data
 )
 
+# import utility functions
 from utils import (
-    getMovie
+    getMovie,
+    getGenreFromId
 )
 
-import base64
-import json
-from io import BytesIO
-import matplotlib.pyplot as plt
-
+#initialize flask app
 def init_app():
     # Create app
     app = Flask(
@@ -33,16 +38,16 @@ def index():
 #visualize route for the oscar's dataset
 @app.route('/visualize')
 def visualize():
-
     sql_query = 'SELECT year, count(*) as count FROM public.sk10189omdb_data group by year;'
     data = get_data(engine, sql_query)
     return render_template('components/visualize.html', data=data.values.tolist())
 
-# not sure rn what to do with it
+# This will be the route for visualizing predictions that we received from the model.
 @app.route('/prediction')
 def prediction():
     return render_template('components/prediction.html')
 
+# Search route for searchin movies.
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     # integrate the tmdb api to get the movies from the search.
@@ -50,7 +55,10 @@ def search():
     movies = None
     if "movie" in args.keys():
         movies = getMovie(args.get('movie'))["results"]
-
+        #movies["genre_ids"] = getGenreFromId(movies["genre_ids"])
+        for movie in movies:
+            movie["genre_ids"] = (', ').join(getGenreFromId(movie["genre_ids"]))
+    
     return render_template('components/search.html', movies= movies, search = args.get('movie') )
 
 @app.route('/about')
--- utils.py
+++ utils.py
@@ -9,3 +9,20 @@ def getMovie( movie ):
     )
     
     return movies
+
+def getGenreFromId( genre_ids ):
+    res = []
+    url = "https://api.themoviedb.org/3/genre/movie/list?language=en"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOThiNWFmYjQyNzNhYTFiNTU1MmE1YjkxMDcxYzFlNiIsInN1YiI6IjYzZjY2OGUwNjljNzBmMDA4MjIxN2FlMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R2oKaK7HGt_aDFwA48zNJFIlGu61lU_eV6HHJgBhJw8"
+    }
+
+    response = requests.get(url, headers=headers).json()
+    
+    for genre in response["genres"]:
+        if genre["id"] in genre_ids:
+            res.append(genre["name"])
+
+    return res
